d6f833ff76a0855046b47f226f34058c816ef41d
Nithyashree Senguttuvan <120423852+nithyashreesenguttuvan@users.noreply.github.com>
2024-08-01 14:53:17 -0700

    Create example_usage.py

---
+++ src/example_usage.py
@@ -0,0 +1,19 @@
+# Example usage:
+base_dir = '/content/sample_data/instance1'
+G, cell_info, guides = load_ispd_data(base_dir)
+visualize_graph(G)
+
+# Example usage:
+base_dir = '/content/sample_data/instance2'
+G, cell_info, guides = load_ispd_data(base_dir)
+visualize_graph(G)
+
+# Example usage:
+base_dir = '/content/sample_data/instance3'
+G, cell_info, guides = load_ispd_data(base_dir)
+visualize_graph(G)
+
+# Example usage:
+base_dir = '/content/sample_data/instance4'
+G, cell_info, guides = load_ispd_data(base_dir)
+visualize_graph(G)
